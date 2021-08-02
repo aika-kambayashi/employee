57f02e7cc376561ef797cabd8cadf1892db99b3e
Aika Kambayashi <kambayashi.aika@no1s.biz>
2021-08-02 15:10:55 +0900

    first-commit

---
+++ models/DepartmentRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+class DepartmentRepository extends DbRepository {
+    public function getAll() {
+        $sql = 'select * from department';
+        return $this->fetchAll($sql);
+    }
+}
